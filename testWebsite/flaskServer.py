74259a88b646cc427f695bc1bcda94e6c4b46b64
nlchoromokos <42478451+nlchoromokos@users.noreply.github.com>
2021-04-26 13:13:40 -0500

    stuff

--- testWebsite/flaskServer.py
+++ testWebsite/flaskServer.py
@@ -9,19 +9,20 @@ app.config['SECRET_KEY'] = '8642a640a492530125e4412552ee0db9'
 def index():
     return render_template('index.html')
 
-'''
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About', )
+
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html', title='Register', )
+    return render_template('register.html')
 
-@app.route('/login')
+
+
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html', title='Login', )
-'''
+    return render_template('login.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
