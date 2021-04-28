bde9afbf7f9aa8b1f0bc2fe1fce06b8e8b135d3f
nlchoromokos <42478451+nlchoromokos@users.noreply.github.com>
2021-04-28 13:20:14 -0500

    login

--- testWebsite/flaskServer.py
+++ testWebsite/flaskServer.py
@@ -19,14 +19,15 @@ def register():
     else:
         return render_template('register.html')
 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    return render_template('login.html')
+
+
 @app.route('/<usr>')
 def user(usr):
     return f'<h1>{usr}</h1>'
 
-'''
-@app.route('/login', methods=['GET','POST'])
-def login():
-    return render_template('login.html')'''
 
 if __name__ == '__main__':
     app.run(debug=True)
