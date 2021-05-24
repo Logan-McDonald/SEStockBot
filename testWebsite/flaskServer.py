4f54be649df9291f743ba541c3a8f65548d07253
nlchoromokos <42478451+nlchoromokos@users.noreply.github.com>
2021-05-24 13:33:53 -0500

    checks to make sure accounts r valid

--- testWebsite/flaskServer.py
+++ testWebsite/flaskServer.py
@@ -14,8 +14,30 @@ def index():
 @app.route('/register', methods=['GET','POST'])
 def register():
     if request.method == "POST":
-        user = request.form['first_name']
-        return redirect(url_for('user', usr=user))
+        fname = request.form['first_name']
+        lname = request.form['last_name']
+        email = request.form['email']
+        password = request.form['password']
+        password1 = request.form['password_repeat']
+
+        if len(fname) < 1:
+            flash('Enter valid First Name', category='error')
+            return redirect(url_for('register'))
+        elif len(lname) < 1:
+            flash('Enter valid Last Name', category='error')
+            return redirect(url_for('register'))
+        elif len(email) < 1:
+            flash('Enter valid email', category='error')
+            return redirect(url_for('register'))
+        elif len(password) < 8:
+            flash('Password must be 8 characters', category='error')
+            return redirect(url_for('register'))
+        elif password != password1:
+            flash('Passwords do not match', category='error')
+            return redirect(url_for('register'))
+        else:
+            flash('Account Created',category='success')
+            return redirect(url_for('landing', usr=fname))
     else:
         return render_template('register.html')
 
@@ -23,10 +45,9 @@ def register():
 def login():
     return render_template('login.html')
 
-
-@app.route('/<usr>')
-def user(usr):
-    return f'<h1>{usr}</h1>'
+@app.route('/landing')
+def landing(usr):
+    return f'<h1> hello , {usr}</h1>'
 
 
 if __name__ == '__main__':
